6e229738581adcdf40d2906e66a0d2e9f2ca1ca7
shimizu-phd <shimizu.phd@gmail.com>
2023-09-26 17:55:20 +0900

    1st

--- BovineLeukosis.py
+++ BovineLeukosis.py
@@ -1,9 +1,6 @@
-import io
-
 import streamlit as st
 from ultralytics import YOLO
 import pandas as pd
-import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import cv2
@@ -12,7 +9,7 @@ import os
 import tempfile
 
 tag = ['N', 'A', 'M', 'G']
-colors = [(0,255,0), (255,0,0), (255,255,0), (0,0,255)]
+colors = [(0, 255, 0), (255, 0, 0), (255, 255, 0), (0, 0, 255)]
 n_class = len(tag)
 img_size = 1280
 n_result = 1
@@ -37,7 +34,7 @@ def plot_image(img_path, boxes, labels, color=None, line_thickness=None):
     for box, label in zip(boxes, labels):
         box = [int(i) for i in box]
         x, y, w, h = box
-        x1, y1, x2, y2 = int(x-w/2), int(y-h/2), int(x+w/2), int(y+h/2)
+        x1, y1, x2, y2 = int(x - w / 2), int(y - h / 2), int(x + w / 2), int(y + h / 2)
         cv2.rectangle(img,
                       pt1=(x1, y1),
                       pt2=(x2, y2),
